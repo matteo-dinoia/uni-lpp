95e995c5841735ef0a8e2e8a9717a738a448584a
Matteo Di Noia <dmatteo002@gmail.com>
2023-09-28 10:27:20 +0200

    adding ex1

---
+++ lab01/anni.hs
@@ -0,0 +1,11 @@
+anno_terra :: Float
+anno_terra = 2 * pi * 150e6
+
+v_terra :: Float
+v_terra = anno_terra / (365 * 24)
+
+anno_mercurio :: Float
+anno_mercurio = 2 * pi * 55e6
+
+v_mercurio :: Float
+v_mercurio =  anno_terra / (88 * 24)
